266faeed6308e965b42ce283066d830d71a7a966
Ray Hayes <ray.hayes@gmail.com>
2017-08-04 14:31:20 +0100

    Initial version of basic web-server

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.AspNetCore.Http.Abstractions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace webserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Directory.GetCurrentDirectory();
+            if (args.Length > 0)
+            {
+                path = args[0];
+            }
+
+            var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseContentRoot(path)
+                    .UseWebRoot(path)
+                    .UseUrls("http://localhost:5000")
+                    .UseStartup<Startup>()
+                    .Build();
+
+            host.Run();
+        }
+    }
+
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Information);
+
+            var fileServerOptions = new FileServerOptions();
+            fileServerOptions.EnableDefaultFiles = true;
+            fileServerOptions.EnableDirectoryBrowsing = true;
+            fileServerOptions.FileProvider = env.WebRootFileProvider;
+            fileServerOptions.StaticFileOptions.ServeUnknownFileTypes = true;
+            app.UseFileServer(fileServerOptions);
+        }
+    }
+}
